ff5156f2ce8c08d5c77da5a757549b10895ee0e4
Miguel Ardila <miguel_sxe@hotmail.com>
2023-05-12 22:05:11 -0500

    js

---
+++ valores.js
@@ -0,0 +1,16 @@
+40 // TIPO DE DATOS NUMERO
+
+"Miguel Ardila" //TIPO DE DATO STRING
+
+true // TIPO DE DATO BOOLEAN
+false // TIPO DE DATO BOOLEAN se usa para validar si tomamos una decicion o no
+
+null // TIPO DE DATO NULL
+undefined // TIPO DE DATO UNDEFINED el null y undefined son valores que no tienen valor alguno y se usan para validar si una variable tiene valor o no
+//estos son valores primitivos
+
+
+//valores no primitivos
+[1,2,3] // TIPO DE DATO ARRAY
+{nombre: "Miguel"} // TIPO DE DATO OBJETO
+
---
+++ variables.js
@@ -0,0 +1,17 @@
+//Una variable es la representación de un lugar que reservamos en memoria para guardar un valor. El valor puede ser cualquier tipo de dato, inclusive objetos o funciones.
+var nombre = "Miguel";
+
+// Las variables se declaran con la palabra reservada var, seguida del nombre que le queremos asignar. El nombre de la variable puede contener letras, números y signos de dólar ($), guión bajo (_); pero no puede comenzar con un número.
+
+// declarar una variable
+var edad;
+//inicializar la variable
+edad = 30;
+
+var elementos = ["Computadora, Celular"];
+
+var persona = {
+    nombre: "Miguel",
+    edad: 30
+}
+elementos [1]
