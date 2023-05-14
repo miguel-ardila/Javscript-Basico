0fb5ff855113bf962a2c14ddb9f8ce6cdf60379c
Miguel Ardila <miguel_sxe@hotmail.com>
2023-05-13 19:02:58 -0500

    funciones

---
+++ funciones.js
@@ -0,0 +1,14 @@
+// hay 2 tipos de fucniones en JS Declarativas y de Expresión
+
+// Declarativas
+function miFuncion() {
+    return 3;
+}
+miFuncion(); //llamado de la funcion
+
+
+// Expresión
+var miFuncion = function(a, b) { //(a, b) son los parametros que recibe la funcion
+    return a + b;
+}
+miFuncion(); //llamado de la funcion
