921193ecb72e2e8dbda4bf14a39b792cac5a7c1f
Miguel Ardila <miguel_sxe@hotmail.com>
2023-05-15 12:36:28 -0500

    visual

--- funciones.js
+++ funciones.js
@@ -6,9 +6,24 @@ function miFuncion() {
 }
 miFuncion(); //llamado de la funcion
 
+//ejemplo de funcion declarativa
+function saludar (nombre) {
+    console.log(`Hola ${nombre}`);
+}
+saludar("amy"); //llamado de la funcion
+
 
 // Expresión
 var miFuncion = function(a, b) { //(a, b) son los parametros que recibe la funcion
     return a + b;
 }
 miFuncion(); //llamado de la funcion
+
+//ejemplo de funcion expresiva
+var nombre = function(nombre) {
+    console.log(`Hola ${nombre}`);
+}
+nombre("amy"); //llamado de la funcion
+
+//Diferencias: A las funciones declarativas se les aplica hoisting, y a la expresión de función, no. Ya que el hoisting solo se aplica en las palabras reservadas var y function.
+//Lo que quiere decir que con las funciones declarativas, podemos mandar llamar la función antes de que ésta sea declarada, y con la expresión de función, no, tendríamos que declararla primero, y después mandarla llamar.
